3fbc4ab00cb0ebc23c888a94b6151815e2e9151a
kingplanet2 <eegodwin2@gmail.com>
2026-04-10 11:38:15 +0100

    Fix CORS origins

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const app = express();
 // MIDDLEWARE
 // ─────────────────────────────────────────────
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://belle-food.onrender.com'],
+  origin: [
+    'http://localhost:5173',
+     'http://localhost:3000',
+      'https://bellefood-project.onrender.com',
+      'https://bellefood-blue.vercel.app'
+    ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization' , 'x-admin-password'],
 }));
